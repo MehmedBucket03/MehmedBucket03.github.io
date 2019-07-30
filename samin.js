8a36b4de7a8bcef5518bfe025fb7fe54e404473e
MehmedBucket03 <47090986+MehmedBucket03@users.noreply.github.com>
2019-07-30 15:40:17 -0400

    Create samin.js

---
+++ samin.js
@@ -0,0 +1,16 @@
+alert("This site can damage your computer! Would you like to continue?");
+
+function bigImg(x) {
+    x.style.height = "400px";
+    x.style.width = "500px";
+}
+
+function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "250px";
+}
+
+function whatTime(x) {
+    time = document.getElementById('darkWeb');
+    time.innerHTML = '<p>DARK WEB</p>'
+}
